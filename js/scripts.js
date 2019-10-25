9c4c3f3cc8713b5efead54c514d17ae301fa50d4
Wei Dai <wei@>
2019-10-25 15:43:50 -0700

    inform that research is still being done at the current moment

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("form.languages").submit(function(event) {
+  $("form#languages").submit(function(event) {
     event.preventDefault();
     var ruby1 = $("#questions option:selected").val();
     var cs1 = $("#questions option:selected").val();
@@ -8,10 +8,22 @@ $(document).ready(function() {
     var all1 = $("#questions option:selected").val();
 
     if ("#questions") {
-
-    }
-    else {
       $(".warning").show();
+    }
+    else if (ruby1==='yes') {
+      $(".r1").show();
+    }
+    else if(cs1==='yes') {
+      $(".c1").show();
+    }
+    else if(python1==='yes') {
+      $(".p1").show();
+    }
+    else if(js1==='yes') {
+      $(".j1").show();
+    }
+    else if(ruby1,cs1,python1,js1==='yes') {
+      $(".a1").show();
     }
   });
 });
