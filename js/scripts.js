372a48397b8c7d0d0691f5002666b859749c037f
Wei Dai <wei@>
2019-10-25 16:28:46 -0700

    figure out error and perfect the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,54 @@
 $(document).ready(function() {
   $("form#languages").submit(function(event) {
     event.preventDefault();
-    var ruby1 = $("#questions option:selected").val();
-    var cs1 = $("#questions option:selected").val();
-    var python1 = $("#questions option:selected").val();
-    var js1 = $("#questions option:selected").val();
-    var all1 = $("#questions option:selected").val();
+    var ruby1 = $("#ruby option:selected").val();
+    var cs1 = $("#cs option:selected").val();
+    var python1 = $("#python option:selected").val();
+    var js1 = $("#js option:selected").val();
+    var all1 = $("#all option:selected").val();
 
-    if ("#questions") {
-      $(".warning").show();
-    }
-    else if (ruby1==='yes') {
+    // if ("#questions") {
+    //   $(".warning").show();
+    // }
+    // else if (ruby1==="yes") {
+    //   $(".r1").show();
+    // }
+    // else if(cs1==='yes') {
+    //   $(".c1").show();
+    // }
+    // else if(python1==='yes') {
+    //   $(".p1").show();
+    // }
+    // else if(js1==='yes') {
+    //   $(".j1").show();
+    // }
+    // else if(ruby1, cs1, python1, js1==='yes') {
+    //   $(".a1").show();
+    // }
+
+    if (ruby1==="yes"){
       $(".r1").show();
     }
-    else if(cs1==='yes') {
+
+    if (cs1==="yes"){
       $(".c1").show();
     }
-    else if(python1==='yes') {
+
+    if (python1==="yes"){
       $(".p1").show();
     }
-    else if(js1==='yes') {
+
+    if (js1==="yes"){
       $(".j1").show();
     }
-    else if(ruby1,cs1,python1,js1==='yes') {
+
+    if (all1==="yes"){
       $(".a1").show();
     }
+
+    if (".questions"){
+      $(".warning").show();
+    }
+
   });
 });
