d34c4a8049612ad3eb3f70ff3b35d241a8b532f6
Wei Dai <wei@>
2019-10-25 14:21:39 -0700

    try to figure out errors in the webpage, still researching

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   $("form.languages").submit(function(event) {
     event.preventDefault();
-    var ruby1 = $("#ruby option:selected").val();
-    var cs1 = $("#cs option:selected").val();
-    var python1 = $("#python option:selected").val();
-    var js1 = $("#js option:selected").val();
-    var all1 = $("#all option:selected").val();
+    var ruby1 = $("#questions option:selected").val();
+    var cs1 = $("#questions option:selected").val();
+    var python1 = $("#questions option:selected").val();
+    var js1 = $("#questions option:selected").val();
+    var all1 = $("#questions option:selected").val();
 
-    if (".questions") {
+    if ("#questions") {
 
     }
     else {
